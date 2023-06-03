2a3c8f112626aef4830b7c31e05c3e59b7121018
sampath11790 <bsampath11790@gmail.com>
2023-06-03 15:13:26 +0530

    database config and user model creation

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const DB = require("./Utli/database");
 const UserRoute = require("./Route/User");
+//models
+const User = require("./Model/User");
 //middleware
 app.use(bodyparser.json({ extended: false }));
 app.use("/", (req, res, next) => {
@@ -15,6 +18,10 @@ app.use(UserRoute);
 app.use((req, res, next) => {
   res.status(404).json({ error: "page note found" });
 });
-app.listen(process.env.PORT, () => {
-  console.log("connected");
-});
+DB.sync()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected");
+    });
+  })
+  .catch((err) => console.log(err));
